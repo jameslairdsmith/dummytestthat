634a6df12a19fd0e94e9d063d78d512db9486605
James Laird-Smith <jameslairdsmith@gmail.com>
2020-08-15 19:00:38 +0100

    added documentation

--- R/inf-series.R
+++ R/inf-series.R
@@ -1,3 +1,10 @@
+#' @title A function to sum an infinite series
+#'
+#' @description This function calculates the sum of an infinite series.
+#'
+#' @param a the first item of the infinite series
+#' @param r the ratio of the subsequent items in the series
+#'
 #' @export
 #' @importFrom assertthat assert_that
 
